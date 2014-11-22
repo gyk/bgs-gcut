639bdcf40684dbb424176a436535076070ce0c97
Yukun Guo <gyk.net@gmail.com>
2014-11-22 17:18:10 +0800

    Add filter and suffix constants to CONFIG

--- CONFIG-sample.m
+++ CONFIG-sample.m
@@ -13,6 +13,26 @@ classdef CONFIG
 	% frames of videos will be saved here
 	SNAPSHOT_PATH = 'F:\silhouette\video\';
 
+	% Trial 1 has been split into two parts, and the first half is used for 
+	% validation and the second half for training.
+	% Since our algorithm does not need validation set, the 'Validate' part 
+	% is treated as testing set.
+	REAL_TR_FILTER = @(h) isequal(h.Trial, '1') && isequal(h.Partition, 'Train');
+	REAL_TR_SUFFIX = 'R_TRAIN';
+
+	REAL_TE_FILTER = @(h) isequal(h.Trial, '1') && isequal(h.Partition, 'Validate');
+	REAL_TE_SUFFIX = 'R_TEST';
+
+	% Trial 2 is reserved for testing, the mocap data for which is withheld to 
+	% prevent parameter tuning. You have to submit the results on their crappy 
+	% website for evaluation. (No thanks, I would not like to use it.)
+
+	% Trial 3 contains only mocap data and is intended to be used by researchers 
+	% interested in learning motion priors.
+	% Here we use trial 3 to produce synthetic data for training.
+	% So obviously there are no `SYNTH_TE_*`.
+	SYNTH_TR_FILTER = @(h) isequal(h.Trial, '3');
+	SYNTH_TR_SUFFIX = 'S_TRAIN';
 	end
 
 	methods (Static)
