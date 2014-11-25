66588a885e5431b121d0a50c00cc13f5a04b294b
Yukun Guo <gyk.net@gmail.com>
2014-11-25 14:11:16 +0800

    Update main.m for VideoStream background subtraction

--- main.m
+++ main.m
@@ -22,3 +22,28 @@ for i = 1:3:nFrames
 	title(['#', num2str(i)])
 	pause;
 end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Test background subtraction in VideoStream
+CONFIG.addPaths();
+hes = HEUtilities.select(CONFIG.REAL_TE_FILTER);
+HEUtilities.extractPoses(hes, CONFIG.REAL_TE_SUFFIX);
+
+i = input(sprintf('Choose a sequence in HumanEva Dataset (1 - %d): ', ...
+	numel(hes)));
+he = hes(i);
+fprintf('Loading sequence #%d (%s, %s, %s)\n', i, ...
+	he.SubjectName, he.ActionType, he.Trial);
+camera = upper(input('Choose the camera: ', 's'));
+vs = VideoStream('heData', he, 'camera', camera);
+vs.modelBackground();
+
+for i = 1:3:vs.nFrames
+	subplot(1, 2, 1);
+	imshow(vs.at(i));
+	subplot(1, 2, 2);
+	imshow(vs.bwAt(i));
+	xlabel(['#', num2str(i)])
+	pause;
+end
