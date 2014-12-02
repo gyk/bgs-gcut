5562b7e41021b7ae4c2f1e6634b510a401187db1
Yukun Guo <gyk.net@gmail.com>
2014-12-02 11:52:29 +0800

    Add makeShapePrior.m

---
+++ makeShapePrior.m
@@ -0,0 +1,10 @@
+function [sp] = makeShapePrior(joints2d, imSize)
+	assert(all(size(joints2d) == [15, 2]));
+	% difference in Y axis indicates the height of the actor
+	yDev = minmax(joints2d(:, 2)');
+	yDev = yDev(2) - yDev(1);
+	sp = SkeletonDrawer.drawAsShapePrior(joints2d, imSize);
+	sp = bwdist(sp);
+	% TODO: choose better parameters
+	sp = 1 ./ (1 + exp(0.08 * (sp - yDev / 18)));
+end
