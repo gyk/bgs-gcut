5005570bc221fcfb9627cd953a744d393c23c2d6
Yukun Guo <gyk.net@gmail.com>
2014-12-04 22:37:48 +0800

    'Box' type should use pre-modelled background too
    And update comments.

--- CONFIG-sample.m
+++ CONFIG-sample.m
@@ -5,28 +5,33 @@ classdef CONFIG
 
 	HE_CODE_PATH = 'F:\HumanEva\Release_Code_v1_1_beta\';
 
-	% Background means and variances, not been used because
-	% some experiments show that using it actually compromises 
+	% Pre-modelled background means and deviations, only used when the actor 
+	% appears almost in the same region of the video.
+	% Some experiments show that using it actually compromises 
 	% the quality of output videos.
 	BG_PATH = 'F:\HumanEva\Background\';
 
 	% frames of videos will be saved here
 	SNAPSHOT_PATH = 'F:\silhouette\video\';
 
-	% NOTE: For now we remove action types 'ThrowCatch' and 'Gestures' because 
-	% we do not use pre-modelled backgrounds and silhouettes of these actions 
-	% cannot be extracted appropriately.
+	% NOTE: For the action types 'ThrowCatch', 'Gestures' and 'Box', 
+	% silhouette extraction must be done using pre-modelled backgrounds.
+	% FIXME: for now simply ignore those actions.
+
+	USE_STATIC_BG = @(h) isequal(h.ActionType, 'ThrowCatch') || ...
+		isequal(h.ActionType, 'Gestures') || ...
+		isequal(h.ActionType, 'Box');
 
 	% Trial 1 has been split into two parts, and the first half is used for 
 	% validation and the second half for training.
 	% Since our algorithm does not need validation set, the 'Validate' part 
 	% is treated as testing set.
 	REAL_TR_FILTER = @(h) isequal(h.Trial, '1') && isequal(h.Partition, 'Train') && ...
-		~isequal(h.ActionType, 'ThrowCatch') && ~isequal(h.ActionType, 'Gestures');
+		~CONFIG.USE_STATIC_BG(h);
 	REAL_TR_SUFFIX = 'R_TRAIN';
 
 	REAL_TE_FILTER = @(h) isequal(h.Trial, '1') && isequal(h.Partition, 'Validate') && ...
-		~isequal(h.ActionType, 'ThrowCatch') && ~isequal(h.ActionType, 'Gestures');
+		~CONFIG.USE_STATIC_BG(h);
 	REAL_TE_SUFFIX = 'R_TEST';
 
 	% Trial 2 is reserved for testing, the mocap data for which is withheld to 
@@ -38,7 +43,7 @@ classdef CONFIG
 	% Here we use trial 3 to produce synthetic data for training.
 	% So obviously there are no `SYNTH_TE_*`.
 	SYNTH_TR_FILTER = @(h) isequal(h.Trial, '3') && ...
-		~isequal(h.ActionType, 'ThrowCatch') && ~isequal(h.ActionType, 'Gestures');
+		~CONFIG.USE_STATIC_BG(h);
 	SYNTH_TR_SUFFIX = 'S_TRAIN';
 	end
 
